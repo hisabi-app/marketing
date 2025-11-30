08bd4b02ebde19f5e0fd377c200b105fa610dd95
Saleem Hadad <saleem_ha@hotmail.com>
2025-11-30 15:37:49 +0400

    wip

--- src/components/blocks/pricing.tsx
+++ src/components/blocks/pricing.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Github } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
